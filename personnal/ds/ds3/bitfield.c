bc18c74029769559c7fb0c60c71254d7a84a11d2
augustin64 <augustin.lucas64@gmail.com>
2021-11-23 22:23:06 +0100

    Ajout entrainement bitfield

---
+++ personnal/ds/ds3/bitfield.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+uint singleton(uint i);
+uint union2(uint s1, uint s2);
+bool has(uint s, uint e);
+uint card(uint s);
+
+int main() {
+    printf("singleton de 4: \t%d\n", singleton(4));
+    printf("union {0,1} et {1,2}: \t%d\n", union2(3, 6));
+    printf("{2, 4} possède 4: \t%d\n", has(20, 4));
+    printf("cardinal de 17: \t%d\n", card(17));
+    return 0;
+}
+
+uint singleton(uint i) {
+      return 1 << i;
+};
+
+uint union2(uint s1, uint s2) {
+    return s1 & s2;
+};
+
+bool has(uint s, uint e) {
+    return union2(s, singleton(e)) != 0;
+};
+
+uint card(uint s) {
+    uint c = 0;
+    while (s != 0) {
+        c++;
+        s = s & (s-1);
+    };
+    return c;
+};
