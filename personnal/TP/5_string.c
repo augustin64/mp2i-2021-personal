e4690d945d2500f71b3c45396409536ff8d8e700
augustin64 <augustin.lucas64@gmail.com>
2021-12-03 11:40:15 +0100

    Update 5_string.c

--- personnal/TP/5_string.c
+++ personnal/TP/5_string.c
@@ -78,16 +78,60 @@ int h(const char* s, int p, int n) {
     int po = 1;
     for (int i=0; i < len; i++) {
         somme = (somme + hash(s[i]) * po)%n;
-        po*=p;
+        po=(po*p)%n;
     };
     return somme%n;
+};
+
+void add(char* k, int v, int* t){
+    t[h(k, 31, 11867)] = v;
+};
+
+int get(char* k, int* t) {
+    return t[h(k, 31, 11867)];
+};
+
+char* sub(char* s, int i, int j) {
+    char* t = (char*)malloc(sizeof(char)*(j-i));
+    for (int k=i; k <= j; k++) {
+        t[k-i] = s[k];
+    };
+    return t;
+};
+
+char* most_frequent_word(char* s, int l) {
+    int t[11867];
+    int n = strlen(s);
+    int freq_max = 0;
+    char* most_word;
+    int val;
+    char* word;
+    for (int i=0; i < 11867; i++) {
+        t[i] = 0;
+    };
+    for (int i=0; i < n-l; i++) {
+        word = sub(s, i, i+l-1);
+        val = get(word, t);
+        val++;
+        add(word, val, t);
+        if (val > freq_max) {
+            freq_max = val;
+            most_word = word;
+        };
+    };
+    return most_word;
 };
 
 int main() {
     printf("true: %d\n", prefix("info", "informatique"));
     printf("true: %d\n", subword("for", "informatique"));
     printf("true: %d\n", anagramme("sacres", "casser"));
-    printf("hash: %d\n", hash('d'));
+    printf("hash: %d\n", hash('s'));
     printf("hash: %d\n", h("patates", 31, 11867));
+    int t[11867];
+    add("patates", 5, t);
+    printf("patates: %d\n", get("patates", t));
+    printf("sub: %s\n", sub("patates", 2, 4));
+    printf("most_freq: %s\n", most_frequent_word("abcbcaabc", 2));
     return 0;
 }
