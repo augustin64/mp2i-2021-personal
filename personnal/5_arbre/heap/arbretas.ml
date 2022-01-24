f522ae2bb564d6709e19e62fb25bafbb0251136b
augustin64 <augustin.lucas64@gmail.com>
2022-01-24 17:38:27 +0100

    arbretas.ml : Ajout `add` et `del`

--- personnal/5_arbre/heap/arbretas.ml
+++ personnal/5_arbre/heap/arbretas.ml
@@ -1,4 +1,6 @@
 #!/usr/bin/ocaml
+type 'a arb = V | N of 'a * 'a arb * 'a arb
+
 let swap t i j =
   let tmp = t.(i) in
   t.(i) <- t.(j);
@@ -6,7 +8,7 @@ let swap t i j =
 
 let shuffle t =
   for i = 0 to Array.length t -1 do
-    swap t i j (Random.int (i+1))
+    swap t i (Random.int (i+1))
 done;;
 
 (*
@@ -40,3 +42,36 @@ Arbre de la question 3:
   (0, 7)    (3, 8)
 
 *)
+
+let rotd = function
+  | N(r, N(gr, gg, gd), d) -> N(gr, gg, N(r, gd, d))
+  | _ -> failwith "Pas de rotation droite possible";;
+
+let rotg = function
+  | N(gr, gg, N(r, gd, d)) -> N(r, N(gr, gg, gd), d)
+  | _ -> failwith "Pas de rotation gauche possible";;
+
+let prio = function
+  | V -> max_int
+  | N((e, p), g, d) -> p;;
+
+let rec add a e = match a with
+  | N(r, g, d) when fst r = fst r -> if snd r > snd e
+              then N(e, a, V)
+              else N(r, N(e, g, V), d)
+  | N(r, g, d) when fst r < fst e -> if prio d < snd e 
+              then rotg ( N(r, g, add d e) )
+              else N(r, g, add d e)
+  | N(r, g ,d) -> if prio g < snd e 
+              then rotd ( N(r, add g e, d) )
+              else N(r, add g e, d)
+  | V -> N(e, V, V);;
+
+let rec del a e = match a with
+  | N(r, g, d) when e < fst r -> N(r, del g e, d)
+  | N(r, g, d) when e > fst r -> N(r, g, del d e)
+  | N(r, V , V) -> V
+  | N(r, g, d) -> if prio g < prio d
+                  then del (rotd a) e
+                  else del (rotg a) e
+  | V -> V;;
