80253f29c4ef24e35975922aaa623b47ff12ec30
augustin64 <augustin.lucas64@gmail.com>
2022-02-16 18:07:45 +0100

    Update Sujet X ENS

--- personnal/sujets/x-ens-2014/arbres_couvrant_de_poids_minimum.ml
+++ personnal/sujets/x-ens-2014/arbres_couvrant_de_poids_minimum.ml
@@ -32,3 +32,47 @@ let liste_aretes_triees m =
   tri (liste_aretes m);;
 
 (* Partie II. Préliminaires sur les arbres *)
+
+(* Question 3)
+   - Cas 1: u et v sont dans des composantes connexes différentes:
+
+     Soit U (resp. V) composante connexe de u (resp. v)
+
+     alors UuV est connexe: en effet si x ∈ U, y ∈ V
+
+     alors  x~u (car U est connexe)
+            y~v (car V est connexe)
+
+     Donc il existe un chemin de x à u auquel on ajoute l'arête {u,v}
+     puis un chemin de v vers y, ce qui donne un
+     chemin entre x et y
+
+     -> UuV est maximal pour C
+     Sinon, il existe W connexe dans G+uv tel que UuV est inclus strictement dans W
+     Il existe w∈W\UuV
+     Comme W est connexe, si x∈UuV alors
+     il existe un chemin C de w à x.
+     Soit y le 1er sommet de UuV le long de C.
+
+     Soit C' la partie de C entre w et y
+     C' n'utilise pas uv donc est un chemin dans G
+
+     Donc w et y étaient dans la même CC dans G
+
+     Ce qui est absurde par maximalité de U et de V.
+     Donc G+uv possède une CC de moins
+
+
+     Si il existe un cycle contenant uv dans G+uv
+     Alors C auquel on enlève uv est un chemin de u à v dans G
+     Donc u et v sont dans la même CC, ce qui est absurde.
+
+   - Cas 2: u et v sont dans la même composante connexe X:
+     Ajouter uv ne change pas le nombre de CC
+     De plus, il y a un chemin C de u à v dans G,
+     qui forme un cycle en ajoutant uv
+ *)
+
+(* Question 5)
+   Récurrence ou parcours (dfs, bfs)
+*)
